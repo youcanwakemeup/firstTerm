7422ed4ac929f07815982f91367d8803c71daa2b
shard <alisher.sharipov2@mail.ru>
2023-04-11 06:01:01 +0600

    .

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-
+        String str = "SkyPro";
+        int halfLength = str.length() / 2;
+        String firstHalf = str.substring(0, halfLength);
+        String secondHalf = str.substring(halfLength);
+        System.out.println(str);
+        System.out.println(firstHalf);
+        System.out.println(secondHalf);
     }
 }
